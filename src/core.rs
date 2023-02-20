0cb6cf9f130a8fe4f1ed446a30435d2e3b36d945
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-20 19:43:44 +0000

    feat: translate format type

--- src/core.rs
+++ src/core.rs
@@ -2,3 +2,4 @@ pub mod config;
 pub mod errors;
 pub mod parser;
 pub mod string_helpers;
+pub mod enums;
---
+++ src/core/enums.rs
@@ -0,0 +1,7 @@
+pub enum FormatType {
+    human = "human",
+    json = "json",
+    yaml = "yaml",
+    github_annotation = "github-annotation",
+    github_annotation_native = "github-annotation-native",
+}

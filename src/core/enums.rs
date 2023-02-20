a0d0993c1d586751af2a413c8247b94e5068928f
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-20 19:47:51 +0000

    feat: add default config

--- src/core/enums.rs
+++ src/core/enums.rs
@@ -1,7 +1,21 @@
+use std::fmt;
+
 pub enum FormatType {
-    human = "human",
-    json = "json",
-    yaml = "yaml",
-    github_annotation = "github-annotation",
-    github_annotation_native = "github-annotation-native",
+    human,
+    json,
+    yaml,
+    github_annotation,
+    github_annotation_native,
+}
+
+impl fmt::Display for FormatType {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            FormatType::human => write!(f, "human"),
+            FormatType::json => write!(f, "json"),
+            FormatType::yaml => write!(f, "yaml"),
+            FormatType::github_annotation => write!(f, "github-annotation"),
+            FormatType::github_annotation_native => write!(f, "github-annotation-native"),
+        }
+    }
 }

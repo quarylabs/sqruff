652705e07f2b695869cedc026fbc05162fe469c6
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-22 10:56:51 +0000

    feat: making progress for rule 001

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,6 @@
 mod core;
 mod rules;
+mod utils;
 
 fn main() {
     println!("Hello, world!");
---
+++ src/utils/reflow/helpers.rs
@@ -0,0 +1,6 @@
+use crate::core::rules::base::{LintFix, LintResult};
+
+/// Return a list of fixes from an iterable of LintResult.
+pub fn fixes_from_results(results: &dyn Iterator<Item = LintResult>) -> Vec<LintFix> {
+    panic!("Not implemented");
+}

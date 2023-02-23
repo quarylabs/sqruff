7e3e0713ee205b0d1839dd9e16df3073ac3fdabc
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-23 10:36:54 +0000

    feat: progress on templaters

---
+++ src/core/templaters/base.rs
@@ -0,0 +1,30 @@
+use crate::core::config::FluffConfig;
+use std::collections::HashMap;
+
+/// Find the indices of all newlines in a string.
+pub fn iter_indices_of_newlines(raw_str: &str) -> impl Iterator<Item = usize> + '_ {
+    // TODO: This may be optimize-able by not doing it all up front.
+    raw_str.match_indices('\n').map(|(idx, _)| idx).into_iter()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_iter_indices_of_newlines() {
+        vec![
+            ("", vec![]),
+            ("foo", vec![]),
+            ("foo\nbar", vec![3]),
+            ("\nfoo\n\nbar\nfoo\n\nbar\n", vec![0, 4, 5, 9, 13, 14, 18]),
+        ]
+        .into_iter()
+        .for_each(|(in_str, expected)| {
+            assert_eq!(
+                expected,
+                iter_indices_of_newlines(in_str).collect::<Vec<usize>>()
+            )
+        });
+    }
+}

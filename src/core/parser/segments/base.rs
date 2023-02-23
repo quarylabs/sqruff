4ae61d6dc536bc81b09ad0989a701e877f8e2050
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-23 11:21:36 +0000

    feat: progress on parser segments

---
+++ src/core/parser/segments/base.rs
@@ -0,0 +1,15 @@
+#[derive(Debug, Clone)]
+pub struct BaseSegment {
+}
+
+
+/// An element of the response to BaseSegment.path_to().
+///     Attributes:
+///         segment (:obj:`BaseSegment`): The segment in the chain.
+///         idx (int): The index of the target within its `segment`.
+///         len (int): The number of children `segment` has.
+pub struct PathStep {
+   segment: BaseSegment,
+    idx: usize,
+    len: usize,
+}

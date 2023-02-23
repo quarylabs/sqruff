be7089a275180e48c49424fac56bfb1de478e8e1
Ben King <9087625+benfdking@users.noreply.github.com>
2023-02-23 10:14:52 +0000

    feat: making progress on types

--- src/core/rules/base.rs
+++ src/core/rules/base.rs
@@ -1,3 +1,11 @@
-pub struct LintResult {}
+pub struct LintResult {
+    pub fix: Vec<LintFix>,
+}
+
+impl Default for LintResult {
+    fn default() -> Self {
+        Self { fix: Vec::new() }
+    }
+}
 
 pub struct LintFix {}

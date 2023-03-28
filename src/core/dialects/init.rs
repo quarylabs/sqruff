4f4481a014e2f95a1a0fe0a919d61c2992e50d37
Ben King <9087625+benfdking@users.noreply.github.com>
2023-03-28 23:11:48 +0100

    feat: implementing basic functionality to work
    [ci skip]

--- src/core/dialects/init.rs
+++ src/core/dialects/init.rs
@@ -4,5 +4,5 @@ pub fn dialect_selector(s: &String) -> Option<Box<dyn Dialect>> {
     match s.as_str() {
         "ansi" => Some(Box::new(crate::dialects::ansi::AnsiDialect {})),
         _ => None,
-    }    
+    }
 }

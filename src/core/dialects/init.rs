d2856662a6ac1b51489409b1b6ea1c45a1b4c2e1
Ben King <9087625+benfdking@users.noreply.github.com>
2023-03-31 17:28:27 +0100

    feat: working on the linter

--- src/core/dialects/init.rs
+++ src/core/dialects/init.rs
@@ -6,3 +6,15 @@ pub fn dialect_selector(s: &String) -> Option<Box<dyn Dialect>> {
         _ => None,
     }
 }
+
+/// Dialect Tuple object for describing dialects.
+pub struct DialectTuple {
+    pub label: String,
+    pub name: String,
+    pub inherits_from: String,
+}
+
+/// Generate a readout of available dialects.
+pub fn dialect_readout<'a>() -> Vec<String> {
+    panic!("Not implemented")
+}

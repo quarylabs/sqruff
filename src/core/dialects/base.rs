3d6584a545972549013a36e9c85b5b6d3ad6384f
Ben King <9087625+benfdking@users.noreply.github.com>
2023-04-12 19:38:32 +0100

    temp

--- src/core/dialects/base.rs
+++ src/core/dialects/base.rs
@@ -1,3 +1,9 @@
+use std::fmt::Debug;
+use crate::core::parser::lexer::Matcher;
+
 pub struct Base {}
 
-pub trait Dialect {}
+pub trait Dialect: Debug {
+    /// Fetch the lexer struct for this dialect.
+    fn get_lexer_matchers(&self) -> Vec<Box<dyn Matcher>>;
+}

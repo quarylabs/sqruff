6c0d6a8d40b341914f2e32cd00b9a608f261251c
Ben King <9087625+benfdking@users.noreply.github.com>
2023-05-05 14:46:10 +0100

    feat: implementing the segments more properly

--- src/core/parser/segments/base.rs
+++ src/core/parser/segments/base.rs
@@ -1,5 +1,6 @@
 use crate::core::parser::markers::PositionMarker;
 use crate::core::rules::base::LintFix;
+use std::fmt::Debug;
 use std::hash::Hash;
 
 /// A stored reference to a fix in the non-templated file.
@@ -22,8 +23,8 @@ pub struct SourceFix {
 ///         idx (int): The index of the target within its `segment`.
 ///         len (int): The number of children `segment` has.
 #[derive(Debug, Clone)]
-pub struct PathStep {
-    pub segment: BaseSegment,
+pub struct PathStep<S: Segment> {
+    pub segment: S,
     pub idx: usize,
     pub len: usize,
 }
@@ -111,8 +112,9 @@ impl AnchorEditInfo {
     }
 }
 
-#[derive(Debug, PartialEq, Clone)]
-pub struct BaseSegment {}
+pub trait SegmentConstructor<SegmentArgs: Debug + Clone> {
+    fn new(raw: &str, pos: &PositionMarker, args: SegmentArgs) -> Self;
+}
 
 pub trait Segment {
     fn get_raw(&self) -> Option<&str> {
@@ -125,15 +127,31 @@ pub trait Segment {
     fn is_code(&self) -> bool;
     fn is_comment(&self) -> bool;
     fn is_whitespace(&self) -> bool;
+    fn is_meta(&self) -> bool {
+        false
+    }
     fn get_default_raw(&self) -> Option<&'static str> {
         None
     }
     fn get_pos_maker(&self) -> Option<PositionMarker> {
         None
     }
+    fn indent_val(&self) -> usize {
+        panic!("Not implemented yet");
+    }
 }
 
+#[derive(Debug, Clone)]
 pub struct CodeSegment {}
+
+#[derive(Debug, Clone)]
+pub struct CodeSegmentNewArgs;
+
+impl SegmentConstructor<CodeSegmentNewArgs> for CodeSegment {
+    fn new(raw: &str, pos: &PositionMarker, args: CodeSegmentNewArgs) -> Self {
+        panic!("Not implemented yet")
+    }
+}
 
 impl Segment for CodeSegment {
     fn get_type(&self) -> &'static str {
@@ -151,7 +169,17 @@ impl Segment for CodeSegment {
 }
 
 /// Segment containing a comment.
+#[derive(Debug, Clone)]
 pub struct CommentSegment {}
+
+#[derive(Debug, Clone)]
+pub struct CommentSegmentNewArgs;
+
+impl SegmentConstructor<CommentSegmentNewArgs> for CommentSegment {
+    fn new(raw: &str, pos: &PositionMarker, args: CommentSegmentNewArgs) -> Self {
+        panic!("Not implemented yet")
+    }
+}
 
 impl Segment for CommentSegment {
     fn get_type(&self) -> &'static str {
@@ -168,86 +196,138 @@ impl Segment for CommentSegment {
     }
 }
 
-/// Segment containing a newline.
-pub struct NewlineSegment {}
-
-impl Segment for NewlineSegment {
-    fn get_type(&self) -> &'static str {
-        "newline"
-    }
-    fn is_code(&self) -> bool {
-        false
-    }
-    fn is_comment(&self) -> bool {
-        false
-    }
-    fn is_whitespace(&self) -> bool {
-        true
-    }
-    fn get_default_raw(&self) -> Option<&'static str> {
-        Some("\n")
-    }
-}
-
-/// Segment containing whitespace.
-pub struct WhitespaceSegment {}
-
-impl Segment for WhitespaceSegment {
-    fn get_type(&self) -> &'static str {
-        "whitespace"
-    }
-    fn is_code(&self) -> bool {
-        false
-    }
-    fn is_comment(&self) -> bool {
-        false
-    }
-    fn is_whitespace(&self) -> bool {
-        true
-    }
-    fn get_default_raw(&self) -> Option<&'static str> {
-        Some(" ")
-    }
-}
-
-/// A placeholder to un-lexable sections.
-///
-/// This otherwise behaves exactly like a code section.
-pub struct RawSegment {}
-
-impl Segment for RawSegment {
-    fn get_type(&self) -> &'static str {
-        "raw"
-    }
-    fn is_code(&self) -> bool {
-        true
-    }
-    fn is_comment(&self) -> bool {
-        false
-    }
-    fn is_whitespace(&self) -> bool {
-        false
-    }
-}
-
-/// A segment used for matching single entities which aren't keywords.
-///
-/// We rename the segment class here so that descendants of
-/// _ProtoKeywordSegment can use the same functionality
-/// but don't end up being labelled as a `keyword` later.
-pub struct SymbolSegment {}
-
-impl Segment for SymbolSegment {
-    fn get_type(&self) -> &'static str {
-        "symbol"
-    }
-    fn is_code(&self) -> bool {
-        true
-    }
-    fn is_comment(&self) -> bool {
-        false
-    }
-    fn is_whitespace(&self) -> bool {
-        false
-    }
-}
+// Segment containing a newline.
+// #[derive(Debug, Clone)]
+// pub struct NewlineSegment {}
+//
+// #[derive(Debug, Clone)]
+// pub struct NewLineSegmentNewArgs;
+//
+// impl Segment<NewLineSegmentNewArgs> for NewlineSegment {
+//     fn new(raw: &str, position_maker: PositionMarker, args: NewLineSegmentNewArgs) -> Self {
+//         panic!("Not implemented yet")
+//     }
+//     fn get_type(&self) -> &'static str {
+//         "newline"
+//     }
+//     fn is_code(&self) -> bool {
+//         false
+//     }
+//     fn is_comment(&self) -> bool {
+//         false
+//     }
+//     fn is_whitespace(&self) -> bool {
+//         true
+//     }
+//     fn get_default_raw(&self) -> Option<&'static str> {
+//         Some("\n")
+//     }
+// }
+//
+// /// Segment containing whitespace.
+// #[derive(Debug, Clone)]
+// pub struct WhitespaceSegment {}
+//
+// #[derive(Debug, Clone)]
+// pub struct WhitespaceSegmentNewArgs;
+//
+// impl Segment<WhitespaceSegmentNewArgs> for WhitespaceSegment {
+//     fn new(raw: &str, position_maker: PositionMarker, args: WhitespaceSegmentNewArgs) -> Self {
+//         panic!("Not implemented yet")
+//     }
+//     fn get_type(&self) -> &'static str {
+//         "whitespace"
+//     }
+//     fn is_code(&self) -> bool {
+//         false
+//     }
+//     fn is_comment(&self) -> bool {
+//         false
+//     }
+//     fn is_whitespace(&self) -> bool {
+//         true
+//     }
+//     fn get_default_raw(&self) -> Option<&'static str> {
+//         Some(" ")
+//     }
+// }
+//
+// /// A placeholder to un-lexable sections.
+// ///
+// /// This otherwise behaves exactly like a code section.
+// #[derive(Debug, Clone)]
+// pub struct RawSegment {}
+//
+// #[derive(Debug, Clone)]
+// pub struct RawSegmentNewArgs;
+//
+// impl Segment<RawSegmentNewArgs> for RawSegment {
+//     fn new(raw: &str, position_maker: PositionMarker, args: RawSegmentNewArgs) -> Self {
+//         panic!("Not implemented yet")
+//     }
+//     fn get_type(&self) -> &'static str {
+//         "raw"
+//     }
+//     fn is_code(&self) -> bool {
+//         true
+//     }
+//     fn is_comment(&self) -> bool {
+//         false
+//     }
+//     fn is_whitespace(&self) -> bool {
+//         false
+//     }
+// }
+//
+// /// A segment used for matching single entities which aren't keywords.
+// ///
+// /// We rename the segment class here so that descendants of
+// /// _ProtoKeywordSegment can use the same functionality
+// /// but don't end up being labelled as a `keyword` later.
+// #[derive(Debug, Clone)]
+// pub struct SymbolSegment {}
+//
+// #[derive(Debug, Clone)]
+// pub struct SymbolSegmentNewArgs;
+//
+// impl Segment<SymbolSegmentNewArgs> for SymbolSegment {
+//     fn new(raw: &str, position_maker: PositionMarker, args: SymbolSegmentNewArgs) -> Self {
+//         panic!("Not implemented yet")
+//     }
+//     fn get_type(&self) -> &'static str {
+//         "symbol"
+//     }
+//     fn is_code(&self) -> bool {
+//         true
+//     }
+//     fn is_comment(&self) -> bool {
+//         false
+//     }
+//     fn is_whitespace(&self) -> bool {
+//         false
+//     }
+// }
+//
+// #[derive(Debug, Clone)]
+// pub struct UnlexableSegment {}
+//
+// #[derive(Debug, Clone)]
+// pub struct UnlexableSegmentNewArgs;
+//
+// impl Segment<UnlexableSegmentNewArgs> for UnlexableSegment {
+//     fn new(raw: &str, position_maker: PositionMarker, args: UnlexableSegmentNewArgs) -> Self {
+//         panic!("Not implemented yet")
+//     }
+//     fn get_type(&self) -> &'static str {
+//         "unlexable"
+//     }
+//     fn is_code(&self) -> bool {
+//         true
+//     }
+//     fn is_comment(&self) -> bool {
+//         false
+//     }
+//     fn is_whitespace(&self) -> bool {
+//         false
+//     }
+// }
--- src/utils/reflow/depth_map.rs
+++ src/utils/reflow/depth_map.rs
@@ -1,4 +1,4 @@
-use crate::core::parser::segments::base::PathStep;
+use crate::core::parser::segments::base::{PathStep, Segment};
 use std::collections::{HashMap, HashSet};
 
 /// An element of the stack_positions property of DepthInfo.
@@ -11,7 +11,7 @@ struct StackPosition {
 
 impl StackPosition {
     /// Interpret a path step for stack_positions.
-    fn stack_pos_interpreter(path_step: &PathStep) -> String {
+    fn stack_pos_interpreter<S: Segment>(path_step: &PathStep<S>) -> String {
         if path_step.idx == 0 && path_step.idx == path_step.len - 1 {
             "solo".to_string()
         } else if path_step.idx == 0 {
@@ -24,7 +24,7 @@ impl StackPosition {
     }
 
     /// Interpret a PathStep to construct a StackPosition
-    fn from_path_step(path_step: &PathStep) -> StackPosition {
+    fn from_path_step<S: Segment>(path_step: &PathStep<S>) -> StackPosition {
         StackPosition {
             idx: path_step.idx,
             len: path_step.len,

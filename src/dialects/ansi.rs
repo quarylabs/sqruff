ca5afaa613307e616ec4aa87dea8c4816b643f3a
Ben King <9087625+benfdking@users.noreply.github.com>
2023-09-27 14:49:36 +0100

    feat: implementing all ANSI lexers but have error

--- src/dialects/ansi.rs
+++ src/dialects/ansi.rs
@@ -1,5 +1,5 @@
 use crate::core::dialects::base::Dialect;
-use crate::core::parser::lexer::{Matcher, RegexLexer};
+use crate::core::parser::lexer::{Matcher, RegexLexer, StringLexer};
 use crate::core::parser::segments::base::{
     CodeSegment, CodeSegmentNewArgs, CommentSegment, CommentSegmentNewArgs, NewLineSegmentNewArgs,
     NewlineSegment, SegmentConstructorFn, WhitespaceSegment, WhitespaceSegmentNewArgs,
@@ -14,6 +14,7 @@ impl Dialect for AnsiDialect {
     }
 }
 
+// TODO This can probably be turned into a const fn
 fn lexer_matchers() -> Vec<Box<dyn Matcher>> {
     vec![
         // Match all forms of whitespace except newlines and carriage returns:
@@ -150,59 +151,303 @@ fn lexer_matchers() -> Vec<Box<dyn Matcher>> {
         //     |(?=\b)              Check that we are at word boundary to avoid matching
         //                          valid naked identifiers (e.g. 123column).
         // )
+        //
+        // NOTE: Instead of using a created LiteralSegment and ComparisonOperatorSegment in the next two,
+        // in Rust we just use a CodeSegment
+        Box::new(
+            RegexLexer::new(
+                "numeric_literal",
+                r"\$(\w*)\$[^\1]*?\$\1\$",
+                &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+                CodeSegmentNewArgs {
+                    code_type: "numeric_literal",
+                },
+                None,
+                None,
+            )
+            .unwrap(),
+        ),
+        Box::new(
+            RegexLexer::new(
+                "like_operator",
+                r"!?~~?\*?",
+                &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+                CodeSegmentNewArgs {
+                    code_type: "like_operator",
+                },
+                None,
+                None,
+            )
+            .unwrap(),
+        ),
+        Box::new(
+            RegexLexer::new(
+                "newline",
+                r"\r\n|\n",
+                &NewlineSegment::new as SegmentConstructorFn<NewLineSegmentNewArgs>,
+                NewLineSegmentNewArgs {},
+                None,
+                None,
+            )
+            .unwrap(),
+        ),
+        Box::new(StringLexer::new(
+            "casting_operator",
+            "::",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "equals",
+            "=",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "greater_than",
+            ">",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "less_than",
+            "<",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "not",
+            "!",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "dot",
+            ".",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "comma",
+            ",",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "comma" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "plus",
+            "+",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "minus",
+            "-",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "divide",
+            "/",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "percent",
+            "%",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "question",
+            "?",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "ampersand",
+            "&",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "vertical_bar",
+            "|",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "caret",
+            "^",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "star",
+            "*",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "bracket_open",
+            "(",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "bracket_close",
+            ")",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "sq_bracket_open",
+            "[",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "sq_bracket_close",
+            "]",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "crly_bracket_open",
+            "{",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "crly_bracket_close",
+            "}",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "colon",
+            ":",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        Box::new(StringLexer::new(
+            "semicolon",
+            ";",
+            &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+            CodeSegmentNewArgs { code_type: "" },
+            None,
+            None,
+        )),
+        // This is the "fallback" lexer for anything else which looks like SQL.
+        Box::new(
+            RegexLexer::new(
+                "code",
+                r"[0-9a-zA-Z_]+",
+                &CodeSegment::new as SegmentConstructorFn<CodeSegmentNewArgs>,
+                CodeSegmentNewArgs { code_type: "code" },
+                None,
+                None,
+            )
+            .unwrap(),
+        ),
     ]
 }
 
 #[cfg(test)]
 mod tests {
     // TODO Implement Test:
-    // use crate::core::config::FluffConfig;
-    // use crate::core::parser::lexer::{Lexer, StringOrTemplate};
-    //
-    // #[test]
-    // fn test_dialect_ansi_file_lex() {
-    //     // Define the test cases
-    //     let test_cases = vec![
-    //         ("a b", vec!["a", " ", "b", ""]),
-    //         ("b.c", vec!["b", ".", "c", ""]),
-    //         (
-    //             "abc \n \t def  ;blah",
-    //             vec!["abc", " ", "\n", " \t ", "def", "  ", ";", "blah", ""],
-    //         ),
-    //     ];
-    //
-    //     for (raw, res) in test_cases {
-    //         // Assume FluffConfig and Lexer are defined somewhere in your codebase
-    //         let config = FluffConfig::new(None, None, None, Some("ansi"));
-    //
-    //         let lexer = Lexer::new(config, None);
-    //
-    //         // Assume that the lex function returns a Result with tokens
-    //         let tokens_result = lexer.lex(StringOrTemplate::String(raw.to_string()));
-    //
-    //         // Check if lexing was successful, and if not, fail the test
-    //         assert!(tokens_result.is_ok(), "Lexing failed for input: {}", raw);
-    //
-    //         let (tokens, errors) = tokens_result.unwrap();
-    //         assert_eq!(errors.len(), 0, "Lexing failed for input: {}", raw);
-    //
-    //         // Check if the raw components of the tokens match the expected result
-    //         let raw_list: Vec<&str> = tokens
-    //             .iter()
-    //             .map(|token| token.get_raw().unwrap())
-    //             .collect();
-    //         assert_eq!(raw_list, res, "Mismatch for input: {}", raw);
-    //
-    //         // Check if the concatenated raw components of the tokens match the original raw string
-    //         let concatenated: String = tokens
-    //             .iter()
-    //             .map(|token| token.get_raw().unwrap())
-    //             .collect();
-    //         assert_eq!(
-    //             concatenated, raw,
-    //             "Concatenation mismatch for input: {}",
-    //             raw
-    //         );
-    //     }
-    // }
+    use crate::core::config::FluffConfig;
+    use crate::core::parser::lexer::{Lexer, StringOrTemplate};
+
+    #[test]
+    fn test_dialect_ansi_file_lex() {
+        // Define the test cases
+        let test_cases = vec![
+            ("a b", vec!["a", " ", "b", ""]),
+            ("b.c", vec!["b", ".", "c", ""]),
+            (
+                "abc \n \t def  ;blah",
+                vec!["abc", " ", "\n", " \t ", "def", "  ", ";", "blah", ""],
+            ),
+        ];
+
+        for (raw, res) in test_cases {
+            // Assume FluffConfig and Lexer are defined somewhere in your codebase
+            let config = FluffConfig::new(None, None, None, Some("ansi"));
+
+            let lexer = Lexer::new(config, None);
+
+            // Assume that the lex function returns a Result with tokens
+            let tokens_result = lexer.lex(StringOrTemplate::String(raw.to_string()));
+
+            // Check if lexing was successful, and if not, fail the test
+            assert!(tokens_result.is_ok(), "Lexing failed for input: {}", raw);
+
+            let (tokens, errors) = tokens_result.unwrap();
+            assert_eq!(errors.len(), 0, "Lexing failed for input: {}", raw);
+
+            // Check if the raw components of the tokens match the expected result
+            let raw_list: Vec<&str> = tokens
+                .iter()
+                .map(|token| token.get_raw().unwrap())
+                .collect();
+            assert_eq!(raw_list, res, "Mismatch for input: {}", raw);
+
+            // Check if the concatenated raw components of the tokens match the original raw string
+            let concatenated: String = tokens
+                .iter()
+                .map(|token| token.get_raw().unwrap())
+                .collect();
+            assert_eq!(
+                concatenated, raw,
+                "Concatenation mismatch for input: {}",
+                raw
+            );
+        }
+    }
 }

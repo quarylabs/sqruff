549f150049b8816c3b4335cfd160edfb2a1d278a
Ben King <9087625+benfdking@users.noreply.github.com>
2023-10-09 21:42:21 +0100

    feat: adding skeleton for dedent and indent

--- src/core/parser/segments.rs
+++ src/core/parser/segments.rs
@@ -1,2 +1,3 @@
 pub mod base;
+pub mod meta;
 pub mod raw;
---
+++ src/core/parser/segments/meta.rs
@@ -0,0 +1,82 @@
+use crate::core::parser::markers::PositionMarker;
+use crate::core::parser::segments::base::Segment;
+
+/// A segment which is empty but indicates where an indent should be.
+///
+///     This segment is always empty, i.e. its raw format is '', but it indicates
+///     the position of a theoretical indent which will be used in linting
+///     and reconstruction. Even if there is an *actual indent* that occurs
+///     in the same place this intentionally *won't* capture it, they will just
+///     be compared later.
+#[derive(Debug, Clone)]
+struct Indent {}
+
+struct IndentNewArgs {}
+
+impl Segment for Indent {
+    fn get_type(&self) -> &'static str {
+        "indent"
+    }
+
+    fn is_code(&self) -> bool {
+        false
+    }
+
+    fn is_comment(&self) -> bool {
+        false
+    }
+
+    fn is_whitespace(&self) -> bool {
+        false
+    }
+}
+
+impl Indent {
+    pub fn new(
+        raw: &str,
+        position_maker: &PositionMarker,
+        args: IndentNewArgs,
+    ) -> Box<dyn Segment> {
+        Box::new(Indent {})
+    }
+}
+
+/// A segment which is empty but indicates where an dedent should be.
+///
+///     This segment is always empty, i.e. its raw format is '', but it indicates
+///     the position of a theoretical dedent which will be used in linting
+///     and reconstruction. Even if there is an *actual dedent* that occurs
+///     in the same place this intentionally *won't* capture it, they will just
+///     be compared later.
+#[derive(Debug, Clone)]
+struct Dedent {}
+
+struct DedentNewArgs {}
+
+impl Segment for Dedent {
+    fn get_type(&self) -> &'static str {
+        "dedent"
+    }
+
+    fn is_code(&self) -> bool {
+        false
+    }
+
+    fn is_comment(&self) -> bool {
+        false
+    }
+
+    fn is_whitespace(&self) -> bool {
+        false
+    }
+}
+
+impl Dedent {
+    pub fn new(
+        raw: &str,
+        position_maker: &PositionMarker,
+        args: DedentNewArgs,
+    ) -> Box<dyn Segment> {
+        Box::new(Dedent {})
+    }
+}

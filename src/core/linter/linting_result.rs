5496a2c550bd35cf5fcd56b108b391ef0171544d
gvozdvmozgu <gvozdvmozgu@gmail.com>
2023-11-16 11:08:50 -0500

    feat: impl `Linter::lint_string_wrapped`

--- src/core/linter/linting_result.rs
+++ src/core/linter/linting_result.rs
@@ -15,4 +15,14 @@ impl LintingResult {
             total_time: 0.0,
         }
     }
+
+    /// Add a new `LintedDir` to this result.
+    pub fn add(&mut self, path: LintedDir) {
+        self.paths.push(path);
+    }
+
+    /// Stop the linting timer.
+    pub(crate) fn stop_timer(&mut self) {
+        self.total_time = self.start_time.elapsed().as_secs_f64();
+    }
 }

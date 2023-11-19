49e7f5fa5e6d2c76b67bca3eb387bb35b68b1669
gvozdvmozgu <gvozdvmozgu@gmail.com>
2023-11-19 12:12:32 -0500

    fix: resolve failing test in `test__linter__empty_file`

--- src/core/dialects/base.rs
+++ src/core/dialects/base.rs
@@ -6,4 +6,20 @@ pub struct Base {}
 pub trait Dialect: Debug {
     /// Fetch the lexer struct for this dialect.
     fn get_lexer_matchers(&self) -> Vec<Box<dyn Matcher>>;
+
+    fn root_segment_name(&self) -> &str {
+        todo!()
+    }
+
+    // Return an object which acts as a late binding reference to the element named.
+    // NB: This requires the dialect to be expanded, and only returns Matchables
+    // as a result.
+    fn r#ref(&self, _name: &str) {
+        todo!()
+    }
+
+    /// Get the root segment of the dialect
+    fn get_root_segment(&self) {
+        self.r#ref(self.root_segment_name())
+    }
 }
--- src/core/parser.rs
+++ src/core/parser.rs
@@ -4,4 +4,5 @@ mod helpers;
 pub mod lexer;
 pub mod markers;
 mod match_result;
+pub mod parser;
 pub mod segments;

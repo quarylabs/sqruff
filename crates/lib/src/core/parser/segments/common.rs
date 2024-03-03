848ee3fbf05d7fd207719147b8318ef8b724f470
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-03-03 11:57:55 -0500

    chore: remove dead code

--- crates/lib/src/core/parser/segments/common.rs
+++ crates/lib/src/core/parser/segments/common.rs
@@ -66,14 +66,3 @@ impl Segment for LiteralSegment {
         ["numeric_literal"].map(ToOwned::to_owned).into_iter().collect()
     }
 }
-
-#[cfg(test)]
-mod test {
-    use crate::core::parser::segments::base::Segment;
-    use crate::core::parser::segments::test_functions::generate_test_segments_func;
-
-    // NOTE: For legacy reasons we override this fixture for this module
-    fn raw_segments() -> Vec<Box<dyn Segment>> {
-        generate_test_segments_func(["bar", "foo", "bar"].to_vec())
-    }
-}

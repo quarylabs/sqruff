626d0e3bff152465cae07f7cbda964c36be2e9b2
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-05-04 06:52:14 +0300

    chore: Add `CV03` and `CV04` rules for convention.rs

--- crates/lib/src/rules/convention.rs
+++ crates/lib/src/rules/convention.rs
@@ -1,10 +1,15 @@
 use crate::core::rules::base::ErasedRule;
 
 pub mod CV02;
+pub mod CV03;
 pub mod CV04;
 
 pub fn rules() -> Vec<ErasedRule> {
     use crate::core::rules::base::Erased as _;
 
-    vec![CV02::RuleCv02::default().erased()]
+    vec![
+        CV02::RuleCV02::default().erased(),
+        CV03::RuleCV03::default().erased(),
+        CV04::RuleCV04::default().erased(),
+    ]
 }

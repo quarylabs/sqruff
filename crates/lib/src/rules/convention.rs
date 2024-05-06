04a506300f7f15a5b6415724c77aae649d76522f
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-05-06 10:16:37 +0300

    chore: Add `CV05` rule

--- crates/lib/src/rules/convention.rs
+++ crates/lib/src/rules/convention.rs
@@ -3,6 +3,7 @@ use crate::core::rules::base::ErasedRule;
 pub mod CV02;
 pub mod CV03;
 pub mod CV04;
+pub mod CV05;
 
 pub fn rules() -> Vec<ErasedRule> {
     use crate::core::rules::base::Erased as _;

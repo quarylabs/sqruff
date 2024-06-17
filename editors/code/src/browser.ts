0d6720be2fcbd78f748940b48bbbd57a8a20e864
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-06-17 02:32:29 -0400

    chore(ci): Add TypeScript workflow for code validation

--- editors/code/src/browser.ts
+++ editors/code/src/browser.ts
@@ -1,21 +1,26 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import { LanguageClient } from "vscode-languageclient/browser";
 
 export function activate(context: vscode.ExtensionContext) {
-    const serverMain = vscode.Uri.joinPath(
-        context.extensionUri,
-        "dist/browserServerMain.js",
+  const serverMain = vscode.Uri.joinPath(
+    context.extensionUri,
+    "dist/browserServerMain.js",
+  );
+
+  const worker = new Worker(serverMain.toString(true));
+  worker.onmessage = (message) => {
+    if (message.data !== "OK") {
+      return;
+    }
+
+    const cl = new LanguageClient(
+      "sqruff-lsp",
+      "Sqruff LSP",
+      { documentSelector: [{ language: "sql" }] },
+      worker,
     );
-
-    const worker = new Worker(serverMain.toString(true));
-    worker.onmessage = (message) => {
-        if (message.data !== "OK") {
-            return;
-        }
-
-        const cl = new LanguageClient("sqruff-lsp", "Sqruff LSP", { documentSelector: [{ language: "sql" }] }, worker);
-        cl.start().then(() => { });
-    };
+    cl.start().then(() => {});
+  };
 }
 
-export function deactivate() { }
+export function deactivate() {}

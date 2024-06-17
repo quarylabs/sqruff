84dba045cfca0e56ffb819da460a7f6717aafa8d
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-06-17 02:32:29 -0400

    chore(ci): Add TypeScript workflow for code validation

--- editors/code/src/lsp-worker.ts
+++ editors/code/src/lsp-worker.ts
@@ -2,28 +2,27 @@ import sqruffInit, * as sqruff_lsp from "../dist/lsp";
 import sqruffWasmData from "../dist/lsp_bg.wasm";
 
 import {
-    createConnection,
-    BrowserMessageReader,
-    BrowserMessageWriter,
-    TextDocumentSyncKind,
-    PublishDiagnosticsParams
+  createConnection,
+  BrowserMessageReader,
+  BrowserMessageWriter,
+  TextDocumentSyncKind,
+  PublishDiagnosticsParams,
 } from "vscode-languageserver/browser";
 
 sqruffInit(sqruffWasmData).then(() => {
-    const reader = new BrowserMessageReader(self);
-    const writer = new BrowserMessageWriter(self);
+  const reader = new BrowserMessageReader(self);
+  const writer = new BrowserMessageWriter(self);
 
-    const connection = createConnection(reader, writer);
+  const connection = createConnection(reader, writer);
 
-    const sendDiagnosticsCallback = (params: PublishDiagnosticsParams) =>
-        connection.sendDiagnostics(params);
+  const sendDiagnosticsCallback = (params: PublishDiagnosticsParams) =>
+    connection.sendDiagnostics(params);
 
-    let lsp = new sqruff_lsp.Wasm(sendDiagnosticsCallback);
+  let lsp = new sqruff_lsp.Wasm(sendDiagnosticsCallback);
 
-    connection.onInitialize(() => lsp.onInitialize());
-    connection.onNotification((...args) => lsp.onNotification(...args));
-    connection.listen();
+  connection.onInitialize(() => lsp.onInitialize());
+  connection.onNotification((...args) => lsp.onNotification(...args));
+  connection.listen();
 
-    self.postMessage("OK");
+  self.postMessage("OK");
 });
-
--- editors/code/src/native.ts
+++ editors/code/src/native.ts
@@ -1,58 +1,54 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 import * as path from "path";
 import { existsSync } from "fs";
 
 import {
-    LanguageClient,
-    ServerOptions,
-    ExecutableOptions,
-    State,
+  LanguageClient,
+  ServerOptions,
+  ExecutableOptions,
+  State,
 } from "vscode-languageclient/node";
 
 const program_extension = process.platform === "win32" ? ".exe" : "";
 
 export function activate(context: vscode.ExtensionContext) {
-    const lspSearchPaths = [
-        path.join(
-            context.extensionPath,
-            "..",
-            "..",
-            "target",
-            "debug",
-            "sqruff" + program_extension,
-        ),
-        path.join(
-            context.extensionPath,
-            "..",
-            "..",
-            "target",
-            "release",
-            "sqruff" + program_extension,
-        ),
-        "sqruff"
-    ];
+  const lspSearchPaths = [
+    path.join(
+      context.extensionPath,
+      "..",
+      "..",
+      "target",
+      "debug",
+      "sqruff" + program_extension,
+    ),
+    path.join(
+      context.extensionPath,
+      "..",
+      "..",
+      "target",
+      "release",
+      "sqruff" + program_extension,
+    ),
+    "sqruff",
+  ];
 
-    let serverModule = lspSearchPaths.find((path) => existsSync(path));
-    if (serverModule === undefined) {
-        console.warn(
-            "Could not locate sqruff server binary",
-        );
-        return;
-    }
+  let serverModule = lspSearchPaths.find((path) => existsSync(path));
+  if (serverModule === undefined) {
+    console.warn("Could not locate sqruff server binary");
+    return;
+  }
 
-    let args = ["lsp"];
-    let serverOptions: ServerOptions = {
-        run: { command: serverModule, options: {}, args: args },
-        debug: { command: serverModule, options: {}, args: args },
-    };
+  let args = ["lsp"];
+  let serverOptions: ServerOptions = {
+    run: { command: serverModule, options: {}, args: args },
+    debug: { command: serverModule, options: {}, args: args },
+  };
 
-    const cl = new LanguageClient(
-        "sqruff-lsp", "Sqruff LSP",
-        serverOptions,
-        { documentSelector: [{ language: "sql" }] }
-    );
+  const cl = new LanguageClient("sqruff-lsp", "Sqruff LSP", serverOptions, {
+    documentSelector: [{ language: "sql" }],
+  });
 
-    cl.start();
+  cl.start();
 }
 
-export function deactivate() { }
+export function deactivate() {}

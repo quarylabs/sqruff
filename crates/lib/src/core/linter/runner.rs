7990a0ff3b6d3368365e3bf4a06d17efe2f5c568
gvozdvmozgu <gvozdvmozgu@gmail.com>
2024-06-30 02:30:57 -0700

    experiment: replace `Mutex<Vec<_>>` with `ConcurrentBag`

--- crates/lib/src/core/linter/runner.rs
+++ crates/lib/src/core/linter/runner.rs
@@ -1,10 +1,15 @@
-use std::sync::Mutex;
+use orx_concurrent_bag::ConcurrentBag;
 
 use super::linted_file::LintedFile;
 use super::linter::Linter;
 
 pub trait Runner: Sized {
-    fn run(&mut self, paths: Vec<String>, fix: bool, linter: &mut Linter) -> Vec<LintedFile>;
+    fn run(
+        &mut self,
+        paths: Vec<String>,
+        fix: bool,
+        linter: &mut Linter,
+    ) -> impl Iterator<Item = LintedFile>;
 }
 
 pub struct RunnerContext<'me, R> {
@@ -19,7 +24,7 @@ impl<'me> RunnerContext<'me, ParallelRunner> {
 }
 
 impl<R: Runner> RunnerContext<'_, R> {
-    pub fn run(&mut self, paths: Vec<String>, fix: bool) -> Vec<LintedFile> {
+    pub fn run(&mut self, paths: Vec<String>, fix: bool) -> impl Iterator<Item = LintedFile> + '_ {
         self.runner.run(paths, fix, self.linter)
     }
 }
@@ -27,18 +32,24 @@ impl<R: Runner> RunnerContext<'_, R> {
 pub struct ParallelRunner;
 
 impl Runner for ParallelRunner {
-    fn run(&mut self, paths: Vec<String>, fix: bool, linter: &mut Linter) -> Vec<LintedFile> {
+    fn run(
+        &mut self,
+        paths: Vec<String>,
+        fix: bool,
+        linter: &mut Linter,
+    ) -> impl Iterator<Item = LintedFile> {
         use rayon::iter::{IntoParallelRefIterator as _, ParallelIterator as _};
 
-        let acc = Mutex::new(Vec::with_capacity(paths.len()));
         let rule_pack = linter.get_rulepack();
+
+        let acc = ConcurrentBag::new();
 
         paths.par_iter().for_each(|path| {
             let rendered = linter.render_file(path.clone());
             let linted_file = linter.lint_rendered(rendered, &rule_pack, fix);
-            acc.lock().unwrap().push(linted_file);
+            acc.push(linted_file);
         });
 
-        acc.into_inner().unwrap()
+        acc.into_inner().into_iter()
     }
 }

85f4c3aec6c443932d8746aa407b14ec93056353
Ben King <9087625+benfdking@users.noreply.github.com>
2024-10-10 11:37:36 -0700

    fix: automatic deploy to vscode

--- .hacking/scripts/download_extension.sh
+++ .hacking/scripts/download_extension.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 set -eo pipefail
 
-VERSION="v$(grep "^version" Cargo.toml | awk -F '"' '{print $2}')"
+VERSION="$(grep "^version" Cargo.toml | awk -F '"' '{print $2}')"
 echo "VERSION: $VERSION"
 
-repo="quarylabs/sqrufff"
+repo="quarylabs/sqruff"
 
-curl -L -o extension.vsix https://github.com/${repo}/releases/download/v"${VERSION}"/editors/code/sqruff-"${VERSION}".vsix
+URL="https://github.com/${repo}/releases/download/v${VERSION}/sqruff-${VERSION}.vsix"
+echo "URL: $URL"
+
+curl -L -o extension.vsix "$URL"

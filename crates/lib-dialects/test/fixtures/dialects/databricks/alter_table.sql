30edcbcb88f72f99dfdbcc75e53f4e794e23bd4e
Ben King <9087625+benfdking@users.noreply.github.com>
2024-12-28 14:39:45 +0000

    chore: introducing some alter table test

---
+++ crates/lib-dialects/test/fixtures/dialects/databricks/alter_table.sql
@@ -0,0 +1,5 @@
+-- ALTER TABLE examples from Databricks documentation
+-- https://docs.databricks.com/en/sql/language-manual/sql-ref-syntax-ddl-alter-table.html
+
+ALTER TABLE Student RENAME TO StudentInfo;
+

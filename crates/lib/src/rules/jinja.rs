b4326da22cadb1372a045f9ce867aac65dcc5104
Ben King <9087625+benfdking@users.noreply.github.com>
2025-02-12 21:00:34 +0000

    temp [ci skip]

--- crates/lib/src/rules/jinja.rs
+++ crates/lib/src/rules/jinja.rs
@@ -1,7 +1,9 @@
+use crate::core::rules::base::ErasedRule;
+
 pub mod jj01;
 
 pub fn rules() -> Vec<ErasedRule> {
     use crate::core::rules::base::Erased as _;
 
-    vec![jj01::RuleJJ01::default().erased()]
+    vec![jj01::RuleJJ01.erased()]
 }

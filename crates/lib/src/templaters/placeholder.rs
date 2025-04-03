9014cc8ee09e4172694c6f260707e435ceb5b8a4
gvozdvmozgu <gvozdvmozgu@gmail.com>
2025-04-03 11:02:25 -0700

    refactor: remove unused API

--- crates/lib/src/templaters/placeholder.rs
+++ crates/lib/src/templaters/placeholder.rs
@@ -720,7 +720,7 @@ param_style = percent
         let sql = "SELECT a,b FROM users WHERE a = %s";
 
         let mut linter = Linter::new(config, None, None, false);
-        let mut result = linter.lint_string_wrapped(sql, None, true);
+        let mut result = linter.lint_string_wrapped(sql, true);
         let result = take(&mut result.paths[0].files[0]).fix_string();
 
         assert_eq!(result, "SELECT\n    a,\n    b\nFROM users WHERE a = %s\n");

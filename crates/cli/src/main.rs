979432e5cb8208f8c3bc11421234dbaea348e217
Andrey Nikolaev <gvozdvmozgu@gmail.com>
2025-06-13 06:33:37 -0700

    Replace jemalloc with mimalloc

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,12 +1,7 @@
 #[cfg(all(
-    not(target_os = "windows"),
     not(target_os = "openbsd"),
-    any(target_arch = "aarch64", target_arch = "powerpc64")
+    any(target_arch = "x86_64", target_arch = "aarch64", target_arch = "powerpc64")
 ))]
-#[global_allocator]
-static ALLOC: jemallocator::Jemalloc = jemallocator::Jemalloc;
-
-#[cfg(target_os = "windows")]
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 

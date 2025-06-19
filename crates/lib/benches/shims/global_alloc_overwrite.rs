31c48f58f9497f94c65b292228ddf87b30de86fc
gvozdvmozgu <gvozdvmozgu@gmail.com>
2025-06-19 19:33:44 -0400

    Replace jemalloc with mimalloc

--- crates/lib/benches/shims/global_alloc_overwrite.rs
+++ crates/lib/benches/shims/global_alloc_overwrite.rs
@@ -1,22 +1,2 @@
-#[cfg(all(
-    not(target_os = "windows"),
-    not(target_os = "openbsd"),
-    any(
-        target_arch = "x86_64",
-        target_arch = "aarch64",
-        target_arch = "powerpc64"
-    )
-))]
-#[cfg(all(
-    not(target_os = "windows"),
-    not(target_os = "openbsd"),
-    any(
-        target_arch = "x86_64",
-        target_arch = "aarch64",
-        target_arch = "powerpc64"
-    )
-))]
-#[allow(non_upper_case_globals)]
-#[unsafe(export_name = "_rjem_malloc_conf")]
-#[allow(unsafe_code)]
-pub static _rjem_malloc_conf: &[u8] = b"dirty_decay_ms:-1,muzzy_decay_ms:-1\0";
+#[global_allocator]
+static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;

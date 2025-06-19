01d4b639e570b12f817f88d50e1e792abe5d7849
Florian Kinder <florian.kinder@enthus.de>
2025-06-20 08:11:54 +0900

    Fixed AL02 for alias after TOP

--- crates/lib/src/rules/aliasing/al02.rs
+++ crates/lib/src/rules/aliasing/al02.rs
@@ -1,5 +1,6 @@
 use ahash::AHashMap;
 use sqruff_lib_core::dialects::syntax::{SyntaxKind, SyntaxSet};
+use sqruff_lib_core::parser::segments::base::ErasedSegment;
 
 use super::al01::{Aliasing, RuleAL01};
 use crate::core::config::Value;
@@ -84,15 +85,67 @@ FROM foo
     }
 
     fn eval(&self, context: &RuleContext) -> Vec<LintResult> {
-        if FunctionalContext::new(context)
+        // For T-SQL, check if this is the equals-style alias syntax
+        if context.dialect.name == sqruff_lib_core::dialects::init::DialectKind::Tsql {
+            // First check: Look for = in the parent SelectClauseElement
+            if let Some(parent) = context.parent_stack.last() {
+                if parent.get_type() == SyntaxKind::SelectClauseElement {
+                    // Look for equals sign in the parent element
+                    let parent_segments = parent.segments();
+                    for segment in parent_segments {
+                        if segment.raw() == "=" {
+                            // This is T-SQL equals-style alias syntax
+                            return Vec::new();
+                        }
+                    }
+                    
+                    // Also check all descendants of the parent for "="
+                    fn has_equals_in_tree(segment: &ErasedSegment) -> bool {
+                        if segment.raw() == "=" {
+                            return true;
+                        }
+                        for child in segment.segments() {
+                            if has_equals_in_tree(child) {
+                                return true;
+                            }
+                        }
+                        false
+                    }
+                    
+                    if has_equals_in_tree(parent) {
+                        return Vec::new();
+                    }
+                }
+            }
+            
+            // Second check: For cases where the parser created an AliasExpression
+            // but it's actually part of a T-SQL equals syntax
+            // Check if the segment immediately after this alias in the parent is "="
+            if let Some(parent) = context.parent_stack.last() {
+                let parent_segments = parent.segments();
+                let mut found_current = false;
+                
+                for segment in parent_segments {
+                    if found_current && segment.raw() == "=" {
+                        // The next segment after our alias is "=", this is T-SQL syntax
+                        return Vec::new();
+                    }
+                    if segment == &context.segment {
+                        found_current = true;
+                    }
+                }
+            }
+        }
+        
+        // Original check - if the last child of the alias expression is "="
+        if let Some(last_child) = FunctionalContext::new(context)
             .segment()
             .children(None)
             .last()
-            .unwrap()
-            .raw()
-            == "="
         {
-            return Vec::new();
+            if last_child.raw() == "=" {
+                return Vec::new();
+            }
         }
 
         self.base.eval(context)

551fee6f264f92ee0c174e312bcc5375088fdc49
Florian Kinder <florian.kinder@enthus.de>
2025-08-01 08:58:17 +0900

    cleanup

--- crates/lib-dialects/src/test_if_lexing.sql
+++
@@ -1,5 +0,0 @@
--- Test 1: Simple IF
-IF 1=1 PRINT 'test'
-
--- Test 2: IF in procedure call position  
-EXEC IF
--- crates/lib-dialects/src/test_if_parse.sql
+++
@@ -1,6 +0,0 @@
-IF 1 <= (SELECT Weight from DimProduct WHERE ProductKey = 1)
-    SELECT ProductKey, EnglishDescription
-    FROM DimProduct WHERE ProductKey = 1
-ELSE
-    SELECT ProductKey, EnglishDescription
-    FROM DimProduct WHERE ProductKey = 1

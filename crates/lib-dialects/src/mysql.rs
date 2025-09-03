50f1580dab9e2915e808d00763b4d1b872326e65
Ben <9087625+benfdking@users.noreply.github.com>
2025-09-03 17:07:11 +0300

    feat(mysql): add div operator functionality

--- crates/lib-dialects/src/mysql.rs
+++ crates/lib-dialects/src/mysql.rs
@@ -1,8 +1,12 @@
 use sqruff_lib_core::dialects::Dialect;
 use sqruff_lib_core::dialects::init::DialectKind;
 use sqruff_lib_core::dialects::syntax::SyntaxKind;
-use sqruff_lib_core::helpers::Config;
+use sqruff_lib_core::helpers::{Config, ToMatchable};
+use sqruff_lib_core::parser::grammar::Ref;
+use sqruff_lib_core::parser::grammar::anyof::one_of;
 use sqruff_lib_core::parser::lexer::Matcher;
+use sqruff_lib_core::parser::node_matcher::NodeMatcher;
+use sqruff_lib_core::vec_of_erased;
 
 use super::ansi;
 
@@ -19,6 +23,35 @@ pub fn raw_dialect() -> Dialect {
         r"(^--|-- |#)[^\n]*",
         SyntaxKind::InlineComment,
     )]);
+
+    mysql.add([
+        (
+            "DivBinaryOperatorSegment".into(),
+            NodeMatcher::new(SyntaxKind::BinaryOperator, |_| {
+                Ref::keyword("DIV").to_matchable()
+            })
+            .to_matchable()
+            .into(),
+        ),
+        (
+            "ArithmeticBinaryOperatorGrammar".into(),
+            one_of(vec_of_erased![
+                Ref::new("PlusSegment"),
+                Ref::new("MinusSegment"),
+                Ref::new("DivideSegment"),
+                Ref::new("MultiplySegment"),
+                Ref::new("ModuloSegment"),
+                Ref::new("BitwiseAndSegment"),
+                Ref::new("BitwiseOrSegment"),
+                Ref::new("BitwiseXorSegment"),
+                Ref::new("BitwiseLShiftSegment"),
+                Ref::new("BitwiseRShiftSegment"),
+                Ref::new("DivBinaryOperatorSegment"),
+            ])
+            .to_matchable()
+            .into(),
+        ),
+    ]);
 
     mysql
 }

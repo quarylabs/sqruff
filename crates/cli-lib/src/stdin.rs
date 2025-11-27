be885157b9747257635f5b184b595683e5515639
gvozdvmozgu <gvozdvmozgu@gmail.com>
2025-11-27 12:42:18 -0800

    refactor: make clippy happy

--- crates/cli-lib/src/stdin.rs
+++ crates/cli-lib/src/stdin.rs
@@ -8,9 +8,9 @@ use std::path::PathBuf;
 ///
 /// The error message is returned if any of the inputs are `-` and there are other inputs.
 pub(crate) fn is_std_in_flag_input(inputs: &[PathBuf]) -> Result<bool, String> {
-    if inputs.len() == 1 && inputs[0] == PathBuf::from("-") {
+    if inputs.len() == 1 && &inputs[0] == "-" {
         Ok(true)
-    } else if inputs.iter().any(|input| *input == PathBuf::from("-")) {
+    } else if inputs.iter().any(|input| input == "-") {
         Err("Cannot mix stdin flag with other inputs".to_string())
     } else {
         Ok(false)

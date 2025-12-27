73945bec8827a3d332792df9fd852a3e6df04495
gvozdvmozgu <gvozdvmozgu@gmail.com>
2025-12-27 06:47:53 -0800

    fix: update syntax kind for StructTypeSegment in BigQuery dialect

--- crates/lib-dialects/src/bigquery.rs
+++ crates/lib-dialects/src/bigquery.rs
@@ -1847,7 +1847,7 @@ pub fn raw_dialect() -> Dialect {
         ),
         (
             "StructTypeSegment".into(),
-            NodeMatcher::new(SyntaxKind::DataType, |_dialect| {
+            NodeMatcher::new(SyntaxKind::StructType, |_dialect| {
                 Sequence::new(vec![
                     Ref::keyword("STRUCT").to_matchable(),
                     Ref::new("StructTypeSchemaSegment")

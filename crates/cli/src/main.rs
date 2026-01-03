3b116ada5113d0844584f2d769755d3e083a7592
Andrey Nikolaev <gvozdvmozgu@gmail.com>
2026-01-03 05:35:00 -0800

    add dhat heap profiling support (#2090)

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,10 +1,25 @@
-#[cfg(target_os = "windows")]
+#[cfg(all(
+    target_os = "windows",
+    not(feature = "dhat-heap"),
+    feature = "mimalloc"
+))]
 #[global_allocator]
 static ALLOC: mimalloc::MiMalloc = mimalloc::MiMalloc;
 
+#[cfg(feature = "dhat-heap")]
+#[global_allocator]
+static ALLOC: dhat::Alloc = dhat::Alloc;
+
 #[cfg(not(feature = "codegen-docs"))]
 pub fn main() {
-    std::process::exit(sqruff_cli_lib::run_with_args(std::env::args_os()));
+    #[cfg(feature = "dhat-heap")]
+    let profiler = dhat::Profiler::new_heap();
+    let exit_code = sqruff_cli_lib::run_with_args(std::env::args_os());
+
+    #[cfg(feature = "dhat-heap")]
+    drop(profiler);
+
+    std::process::exit(exit_code);
 }
 
 #[cfg(feature = "codegen-docs")]

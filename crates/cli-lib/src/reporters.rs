13cfd540b74fbcfb24b321ee20016470eb1f80f2
gvozdvmozgu <gvozdvmozgu@gmail.com>
2026-06-01 12:16:09 +0000

    fix(cli-lib): remove default() on unit struct to satisfy clippy

--- crates/cli-lib/src/reporters.rs
+++ crates/cli-lib/src/reporters.rs
@@ -24,7 +24,7 @@ impl Reporter {
         match format {
             Format::Human => Self::Human(HumanReporter::new(config)),
             Format::GithubAnnotationNative => Self::Github(GithubReporter::new()),
-            Format::Json => Self::Json(JsonReporter::default()),
+            Format::Json => Self::Json(JsonReporter),
             Format::None => Self::None,
         }
     }

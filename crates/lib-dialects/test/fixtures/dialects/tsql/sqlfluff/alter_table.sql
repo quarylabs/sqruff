e408aeeb1df69ca1eafccb4cb35b6ff4ed6b208f
Ben King <9087625+benfdking@users.noreply.github.com>
2026-09-07 13:55:59 +0100

    fix(tsql): support both reference actions (6346)

--- crates/lib-dialects/test/fixtures/dialects/tsql/sqlfluff/alter_table.sql
+++ crates/lib-dialects/test/fixtures/dialects/tsql/sqlfluff/alter_table.sql
@@ -1,3 +1,10 @@
 ALTER TABLE dbo.Products ADD RetailValue AS [QtyAvailable] * UnitPrice * 1.5 PERSISTED; GO
 ALTER TABLE dbo.Products ADD RetailValue AS (QtyAvailable * [UnitPrice] * 1.5) PERSISTED NOT NULL; GO
 ALTER TABLE dbo.Products ADD InventoyDate AS CAST([InventoryTs] AS date); GO
+
+ALTER TABLE [HangFire].[JobParameter]
+ADD CONSTRAINT [FK_HangFire_JobParameter_Job]
+FOREIGN KEY ([JobId])
+REFERENCES [HangFire].[Job] ([Id])
+ON UPDATE CASCADE
+ON DELETE CASCADE; GO

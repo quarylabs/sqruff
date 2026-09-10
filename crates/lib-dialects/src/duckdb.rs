e9207f2e8cf7561f989f60e554817fb22c44c27d
Ben <9087625+benfdking@users.noreply.github.com>
2026-09-10 03:17:51 +0100

    feat(duckdb): support MAP data types (6478)
    Ported from SQLFluff 0135402fb32dd5521614aaca618b822d41104a27
    
    https://github.com/sqlfluff/sqlfluff/pull/6478
    https://github.com/sqlfluff/sqlfluff/commit/0135402fb32dd5521614aaca618b822d41104a27

--- crates/lib-dialects/src/duckdb.rs
+++ crates/lib-dialects/src/duckdb.rs
@@ -43,6 +43,7 @@ pub fn raw_dialect() -> Dialect {
     duckdb_dialect.add_keyword_to_set("unreserved_keywords", "ANTI");
     duckdb_dialect.add_keyword_to_set("unreserved_keywords", "ASOF");
     duckdb_dialect.add_keyword_to_set("unreserved_keywords", "MACRO");
+    duckdb_dialect.add_keyword_to_set("unreserved_keywords", "MAP");
     duckdb_dialect.add_keyword_to_set("unreserved_keywords", "POSITIONAL");
     duckdb_dialect.add_keyword_to_set("unreserved_keywords", "SEMI");
     duckdb_dialect.add_keyword_to_set("unreserved_keywords", "STRUCT");
@@ -165,6 +166,29 @@ pub fn raw_dialect() -> Dialect {
                         .to_matchable(),
                     ])
                     .to_matchable(),
+                ])
+                .to_matchable()
+            })
+            .to_matchable()
+            .into(),
+        ),
+        (
+            "MapTypeSegment".into(),
+            NodeMatcher::new(SyntaxKind::MapType, |_| {
+                Sequence::new(vec![
+                    Ref::keyword("MAP").to_matchable(),
+                    Ref::new("MapTypeSchemaSegment").optional().to_matchable(),
+                ])
+                .to_matchable()
+            })
+            .to_matchable()
+            .into(),
+        ),
+        (
+            "MapTypeSchemaSegment".into(),
+            NodeMatcher::new(SyntaxKind::MapTypeSchema, |_| {
+                Bracketed::new(vec![
+                    Delimited::new(vec![Ref::new("DatatypeSegment").to_matchable()]).to_matchable(),
                 ])
                 .to_matchable()
             })

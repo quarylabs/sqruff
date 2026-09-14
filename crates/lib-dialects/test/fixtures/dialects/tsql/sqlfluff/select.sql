25649326d5e69871f9af714e04e76ac863a00b24
Ben King <9087625+benfdking@users.noreply.github.com>
2026-09-14 15:53:02 +0200

    feat: support Unicode letters in T-SQL identifiers (6952)
    Ported from SQLFluff 77c1fddd5ccff3c6d26386cb7bcb1fea3018d18e
    https://github.com/sqlfluff/sqlfluff/pull/6952
    https://github.com/sqlfluff/sqlfluff/commit/77c1fddd5ccff3c6d26386cb7bcb1fea3018d18e

--- crates/lib-dialects/test/fixtures/dialects/tsql/sqlfluff/select.sql
+++ crates/lib-dialects/test/fixtures/dialects/tsql/sqlfluff/select.sql
@@ -11,3 +11,6 @@ SELECT test(default, 2);
 
 SELECT CURRENT_USER, SESSION_USER, SYSTEM_USER, USER, test(default, 2)
 FROM dbo.all_pop;
+
+-- naked identifier with extended Unicode characters
+select field1 AS 日期差多少天;
